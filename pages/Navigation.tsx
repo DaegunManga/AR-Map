bf5132283b73c7644c87275866815792c2e5e9e3
lukekeum <lukekeum11@gmail.com>
2023-07-17 09:09:46 +0900

    fix: change context to props

--- pages/Navigation.tsx
+++ pages/Navigation.tsx
@@ -1,21 +1,26 @@
-import {useContext, useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {LocContext} from '../app';
 import {
   HeadingTo,
   Waypoint as WaypointType,
   Waypoints,
 } from '../utils/waypoints';
 import Geolocation from '@react-native-community/geolocation';
-import {BackHandler} from 'react-native';
 import {ViroARSceneNavigator} from '@viro-community/react-viro';
 import Navigator from '../components/AR/Navigator';
 import Arrow from '../components/AR/Arrow';
 import Waypoint from '../components/AR/Waypoint';
 import WaypointUtils from '../utils/Waypoint.class';
 
-export default function Navigation() {
-  const navigation = useContext(LocContext);
+interface NavigationProps {
+  info: {
+    loc: '대건고' | '대건중' | '대건학사' | '체육관';
+    waypoints: Waypoints;
+  };
+}
+
+export default function Navigation({info}: NavigationProps) {
+  const navigation = info;
   const [directions, setDirections] = useState<Waypoints>([]);
   const [locationInfo, setLocationInfo] = useState<LocationInfo>({
     meter: 0,
@@ -28,29 +33,40 @@ export default function Navigation() {
   });
 
   useEffect(() => {
-    setDirections(navigation.waypoints);
     const watchGeoID = Geolocation.watchPosition(
       loc => {
         const {latitude, longitude} = loc.coords;
         setCurrentLoc({type: 'waypoint', latitude, longitude});
       },
-      () => BackHandler.exitApp(),
+      e => {
+        console.error(e);
+      },
+      {
+        distanceFilter: 0,
+      },
     );
+    setDirections(navigation.waypoints);
 
     return () => {
       Geolocation.clearWatch(watchGeoID);
     };
-  }, []);
+  });
 
   useEffect(() => {
+    // console.log('Location Changed');
     const waypoint = directions[0];
+    if (!waypoint) return;
     if (WaypointUtils.isInRange(waypoint, currentLoc)) {
       setDirections(() => directions.filter(d => waypoint !== d));
     }
-    setLocationInfo(p => ({
-      direction: WaypointUtils.headingTo(directions, currentLoc),
-      meter: WaypointUtils.getDistance(waypoint.waypoint1, currentLoc),
-    }));
+    console.log(currentLoc);
+    setLocationInfo(prev => {
+      console.log(prev);
+      return {
+        direction: WaypointUtils.headingTo(directions, currentLoc),
+        meter: WaypointUtils.getDistance(waypoint.waypoint1, currentLoc),
+      };
+    });
   }, [currentLoc]);
 
   return (
